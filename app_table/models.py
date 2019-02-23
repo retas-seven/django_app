81349840e7794d80bda010ef41ed4e06d8b4c8b0
t-osawa <retas_seven@hotmail.co.jp>
2019-02-23 11:40:19 +0900

    納品登録用のモデルを作成

--- app_table/models.py
+++ app_table/models.py
@@ -5,9 +5,9 @@ class BaseModel(models.Model):
     '''
     各モデルの共通項目
     '''
-    regist_date = models.DateTimeField(verbose_name='登録日付')
+    regist_date = models.DateTimeField(verbose_name='登録日時')
     regist_user = models.CharField(verbose_name='登録ユーザ', max_length=50)
-    update_date = models.DateTimeField(verbose_name='更新日付', null=True)
+    update_date = models.DateTimeField(verbose_name='更新日時', null=True)
     update_user = models.CharField(verbose_name='更新ユーザ', max_length=50, null=True)
 
     class Meta:
@@ -22,10 +22,38 @@ class Shohin(BaseModel):
     shohin_name = models.CharField(verbose_name='商品名', max_length=100)
     zaikosu = models.IntegerField(verbose_name='在庫数')
     price = models.IntegerField(verbose_name='単価')
-    memo = models.TextField(verbose_name='メモ', max_length=1000)
+    memo = models.TextField(verbose_name='メモ', max_length=1000, null=True)
     
     def __str__(self):
-        return '{}, {}, {}, {}'.format(self.kataban, self.zaikosu, self.price, self.shohin_name)
+        return '{}, {}, {}, {}, {}'.format(self.belong_user, self.kataban, self.zaikosu, self.price, self.shohin_name)
 
     class Meta:
         unique_together=(('belong_user', 'kataban'))
+
+class Nohin(BaseModel):
+    '''
+    納品
+    '''
+    belong_user = models.CharField(verbose_name='所属ユーザ', max_length=50)
+    nohin_date = models.DateField(verbose_name='納品日')
+    totalPrice = models.IntegerField(verbose_name='合計金額')
+    memo = models.TextField(verbose_name='メモ', max_length=1000, null=True)
+    
+    def __str__(self):
+        return '{}, {}'.format(self.belong_user, self.nohin_date)
+
+class NohinDetail(BaseModel):
+    '''
+    納品詳細
+    '''
+    belong_user = models.CharField(verbose_name='所属ユーザ', max_length=50)
+    kataban = models.CharField(verbose_name='型番', max_length=50)
+    price = models.IntegerField(verbose_name='単価')
+    amount = models.IntegerField(verbose_name='数量')
+
+    # 外部キー
+    nohin = models.ForeignKey(Nohin, verbose_name='納品', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{}, {}, {}, {}'.format(self.belong_user, self.kataban, self.price, self.amount)
+
