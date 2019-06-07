4edb67dc7f8553c7a6ac5dcf8eac5b85b39834a9
t-osawa <retas_seven@hotmail.co.jp>
2019-06-08 07:54:00 +0900

    変数名見直し

--- nohin/views/nohin_regist_view.py
+++ nohin/views/nohin_regist_view.py
@@ -34,21 +34,21 @@ class NohinRegistView(LoginRequiredMixin, View):
         納品登録画面-登録処理
         '''
         service = NohinService(self.request)
-        registForm = NohinForm(request.POST)
-        registDetailFormset = NohinDetailFormset(request.POST)
+        form = NohinForm(request.POST)
+        detailFormset = NohinDetailFormset(request.POST)
 
-        if (not registForm.is_valid()) or (not registDetailFormset.is_valid()):
+        if (not form.is_valid()) or (not detailFormset.is_valid()):
             params = {
-                'form': registForm,
-                'formset': registDetailFormset,
+                'form': form,
+                'formset': detailFormset,
                 'shohin_json': service.retrieveShohin(),
                 'company_json': service.retrieveCompany(),
             }
             return render(request, 'nohin/regist.html', params)
 
         # 納品を登録する
-        service.registCompany(registForm.cleaned_data['nohinsaki'])
-        service.registNohin(registForm, registDetailFormset)
+        service.registCompany(form.cleaned_data['nohinsaki'])
+        service.registNohin(form, detailFormset)
         messages.success(request, '納品情報を登録しました。')
 
         # 納品登録画面初期表示処理へリダイレクト
